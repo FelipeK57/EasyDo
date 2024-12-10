744d3a31d5782c739d4ce2c6533f381743234386
Kevin Felipe Bolaños Orozco <kevinboor5725@gmail.com>
2024-12-09 19:13:26 -0500

    add responsive sidebar and drawer components to dashboard

---
+++ src/components/Global/Sidebar.jsx
@@ -0,0 +1,9 @@
+function Sidebar() {
+  return (
+    <aside className="bg-slate-100 min-h-screen p-4">
+      <h1>Sidebar</h1>
+    </aside>
+  );
+}
+
+export default Sidebar;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,19 @@
+import MyDrawer from "../components/Global/myDrawer";
+import Sidebar from "../components/Global/Sidebar";
+
 function Dashboard() {
   return (
-    <main>
-      <h1>Dashboard</h1>
+    <main className="bg-slate-50 md:grid md:grid-cols-[30%_70%]">
+      <div className="block md:hidden">
+        <MyDrawer />
+      </div>
+      <div className="hidden md:block">
+        <Sidebar />
+      </div>
+      <section className="p-4">
+        <h1>Dashboard</h1>
+      </section>
     </main>
   );
 }
-
 export default Dashboard;
